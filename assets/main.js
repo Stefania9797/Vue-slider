c912c793fe06239f60e6397e0a0ea3233b535250
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-18 15:06:27 +0200

    add img circle position

--- assets/main.js
+++ assets/main.js
@@ -7,32 +7,20 @@ const app = new Vue({
             "./assets/img/panorama.jpg",
             "./assets/img/pc.jpg",
             "./assets/img/cane.jpg",
-            
         ],
-       
-
     },
     methods:{
         prev(){
-            // do something
-            console.log('Cliccato su prev');
             if(this.counter === 0){   
                 return this.counter = this.images.length -1
             }
             return this.counter -= 1
-
         },
         next(){
-            // do something
-            console.log('Cliccato su next');
-            
             if(this.counter === this.images.length -1){
-               
                 return this.counter = 0
             }
             return this.counter += 1
-
-        }
-
+        },
     }
 })
